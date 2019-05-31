912464326352685356d7252901176287b43a6f20
syn <vicdemilly@gmail.com>
2019-05-31 10:04:59 +0200

    correction

--- exo2/index.php
+++ exo2/index.php
@@ -19,21 +19,20 @@
                 <p class="lead">Créer une variable isEasy de type booléan et l'initialiser avec une valeur.
                 Afficher C'est facile!! si c'est vrai. Dans le cas contraire afficher C'est difficile !!!.
                 Bonus : L'écrire de deux manières différentes.</p>
-                <?php
-                $isEasy = true;
-                if ($isEasy){
-                  echo 'C\'est facile !!';
-                }
-                else{
-                  echo 'C\'est difficile !!!';
-                }
-                ?>
               </div>
             </div>
           </header>
           <!-- main -->
           <main>
-
+            <?php
+            $isEasy = true;
+            if ($isEasy){
+              echo 'C\'est facile !!';
+            }
+            else{
+              echo 'C\'est difficile !!!';
+            }
+            ?>
           </main>
           <!-- footer -->
           <footer>
